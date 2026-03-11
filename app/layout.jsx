cb8fe1be5fdc4f4d8456222c3ba10eb71baa9539
Bradley Beck <bradleybeck@iMac.fios-router.home>
2026-03-11 16:30:19 -0400

    Disable search indexing

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,9 @@
 export const metadata = {
-  title: "Supports Broker Notes"
+  title: "Supports Broker Notes",
+  robots: {
+    index: false,
+    follow: false,
+  },
 }
 
 export default function RootLayout({ children }) {
